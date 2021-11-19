035bfa135962d6f7dcc1460ba50f84c38243d814
SuperCoconute <bezombes.corentin@gmail.com>
2021-11-19 12:39:35 +0100

    update memo 5

--- preprod/RD/memo_5.sh
+++ preprod/RD/memo_5.sh
@@ -8,34 +8,57 @@ START=1;
 #echo "[+] $START"
 while (($START <= $END))
 do
-		FOLDER=$(ls TASK/)
-        BUFFER=("${(f)$(echo $FOLDER)}")
-        echo $START $(ls TASK |awk '{if(NR=='$START') print $0}'); 
-((START++))
+	FOLDER=$(ls TASK/)
+	BUFFER=("${(f)$(echo $FOLDER)}")
+	echo "[ $START ] $(ls TASK |awk '{if(NR=='$START') print $0}')"; 
+	((START++))
 done
 }
 
-function takeTask(){
-echo "[!] Give the task number"
-read TASK
-VAR=$(ls TASK |awk '{if(NR=='$TASK') print $0}')
-MEMO=("$VAR" "${MEMO[@]}")
+function addTask(){
+	memoTask
+	echo "[ ! ] Give the task number"
+	read TASK
+	VAR=$(ls TASK |awk '{if(NR=='$TASK') print $0}')
+	MEMO=("$VAR" "${MEMO[@]}")
 
+}
+
+function rmTask(){
+	memoTask
+	echo "[ ? ] give me rm name task_1.sh"
+	read rm
+	MEMO[(r)$rm]=()
+}
+
+function choice(){
+	echo "[ ! ] Do you whant add task or remove"
+	echo "[ ! ] 1) Add 2) Remove"
+	read AddRm
+	if (($AddRm == 1))
+	then
+		clear
+		showTask
+		addTask
+	else (($AddRm == 2))
+		clear
+		showTask
+		rmTask
+	fi
 }
 
 function memoTask(){
-echo $MEMO   
+	echo $MEMO   
 }
 
 function main(){
-while [ 1 ]
-do
-    clear
-    showTask
-    memoTask
-    takeTask
-    
-done
+	while [ 1 ]
+	do
+		clear
+		showTask
+		memoTask
+		choice
+	done
 }
 
 main
