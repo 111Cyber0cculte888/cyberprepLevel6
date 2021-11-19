0194628e2f85b62d7ab644866d255efa892dcc94
SuperCoconute <bezombes.corentin@gmail.com>
2021-11-19 13:52:20 +0100

    pre operation memo

---
+++ preprod/RD/src/memo_5.sh
@@ -0,0 +1,89 @@
+MEMO=''
+clear
+
+funtion showTask(){
+END=$(ls TASK |wc -l);
+START=1;
+while (($START <= $END))
+do
+	FOLDER=$(ls TASK/)
+	BUFFER=("${(f)$(echo $FOLDER)}")
+	echo "[ $START ] $(ls TASK |awk '{if(NR=='$START') print $0}')"; 
+	((START++))
+done
+}
+
+function addTask(){
+	memoTask
+	echo "[ ! ] Give the task number"
+	read TASK
+	VAR=$(ls TASK |awk '{if(NR=='$TASK') print $0}')
+	MEMO=("$VAR" "${MEMO[@]}")
+
+}
+
+function rmTask(){
+	memoTask
+	echo "[ ? ] give me rm name task_1.sh"
+	read rm
+	MEMO[(r)$rm]=()
+}
+
+
+function execTask(){
+echo "[+] Task execution started"
+
+LIST_TASK=$(memoTask)
+#echo $LIST_TASK|sed 's/ /\n/g' 
+
+END_TASK=$(echo $LIST_TASK|sed 's/ /\n/g' |wc -l);
+START_TASK=1;
+while (($START_TASK <= $END_TASK))
+do
+	echo "[ $START_TASK ] $(echo $LIST_TASK |sed 's/ /\n/g' |awk '{if(NR=='$START_TASK') print $0}')"; 
+	zsh TASK/$(echo $LIST_TASK |sed 's/ /\n/g' |awk '{if(NR=='$START_TASK') print $0}');
+	((START_TASK++))
+done
+
+	
+
+}
+
+
+function choice(){
+	echo "[ ! ] Do you whant add task or remove"
+	echo "[ ! ] 1) Add 2) Remove 3) Execute"
+	read AddRm
+	if (($AddRm == 1))
+	then
+		clear
+		showTask
+		addTask
+	elif (($AddRm == 2))
+	then
+		clear
+		showTask
+		rmTask
+	else (($AddRm == 3))
+		execTask
+		read
+	fi
+}
+
+function memoTask(){
+	echo $MEMO  |sed 's/ /\n/g' |tac
+}
+
+function main(){
+	while [ 1 ]
+	do
+		clear
+		showTask
+		memoTask
+		choice
+	done
+}
+
+main
+
+
--- preprod/RD/src/task.sh
+++ preprod/RD/src/task.sh
@@ -1,13 +1,41 @@
-function memo(){
-	echo "task 1"
-	echo "task 2"
-	#read
-	if (( $1 == 1 ));
-	then
-		echo "task 1 executed";
-	else
-		echo "task 2 executed";
-	fi
-	memo $1	
+
+
+
+	funtion showTask(){
+	x=$(ls TASK/);echo $x
+	z=$(x=$(ls TASK/);echo $x)
+	buffer=("${(w)$(echo $z)}")
 }
-memo
+
+select='[+]'
+noselect='[-]'
+
+
+task=$(showTask)
+echo '[!] Your choice'
+echo $task
+lineNbr=$(echo $task | wc -l)
+echo '[+] Show nbr line'
+echo $lineNbr
+
+while [ 1 ];
+do
+i=0;
+i=$i+1;
+while (($i <= $lineNbr))
+do
+
+	#	echo '[ ]  '$(echo $task | awk '{if(NR=='$i') print $0}')''
+        
+        echo '[+] Give me line choice '
+        read choice
+        if (($choice <=  $lineNbr))
+
+
+
+	((i++))
+done 
+done
+
+
+
